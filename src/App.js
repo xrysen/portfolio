6b1a08310fa5cf29a2ea284350fd1a893e62da3f
Sean Oyler <xrysen@gmail.com>
2021-02-16 16:54:21 -0800

    Adds material-ui:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
+import Intro from "./Components/Intro"
+import { Fade } from "@material-ui/core";
 
 function App() {
   return (
+  <Fade in={true}>
     <div className = "container">
       <Navbar />
-
+      <Intro />
     </div>
+  </Fade>
   );
 }
 
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className = "intro">
+      <h1>Hi, I'm Sean.</h1>
+      <p className="intro-text">I'm a Full Stack Web Developer from Vancouver Island, Canada. I love solving problems through code, making games and composing music.</p>
+      <p className = "intro-button">My Projects</p>
+    </div>
+  )
+}
+
+export default Intro;
