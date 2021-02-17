e7b41415a36cc9d39ef52b6ab08c546fab5ae946
Sean Oyler <xrysen@gmail.com>
2021-02-16 18:22:29 -0800

    Adds social icons

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Intro = (props) => {
   return (
     <div className = "intro">
       <h1>Hi, I'm Sean.</h1>
-      <p className="intro-text">I'm a Full Stack Web Developer from Vancouver Island, Canada. I love solving problems through code, making games and composing music.</p>
+      <p className="intro-text">I'm a Full Stack Developer from Vancouver Island, Canada. I love solving problems through code, making games and composing music.</p>
       <p className = "intro-button" onClick={props.onClick}>My Projects</p>
+
+      <div>
+        <FontAwesomeIcon icon = {faEnvelope} className = "contact-icons" onClick = {()=> window.open("http://www.google.com")} />
+        <FontAwesomeIcon icon = {faGithub} className = "contact-icons" onClick = {()=> window.open("https://github.com/xrysen")} />
+        <FontAwesomeIcon icon = {faLinkedin} className = "contact-icons" onClick = {() => window.open("https://www.linkedin.com/in/sean-oyler-20337370/")} />
+      </div>
     </div>
   )
 }
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Navigation = (props) => {
   return (
     <div className = "navigation">
-      <a onClick = {props.portfolio} className='nav-link'>Portfolio</a>
-      <a href = "#" className='nav-link'>About</a>
-      <a href = "#" className='nav-link'>Contact</a>
-      <a href = "https://xrysen.github.io/resume/" target="new" className="nav-link">Resume</a>
+      <span className='nav-link'>Portfolio</span>
+      <span className='nav-link'>About</span>
+      <span className='nav-link'>Contact</span>
+      <span className="nav-link" onClick={()=> window.open("https://xrysen.github.io/resume/")}>Resume</span>
     </div>
   )
 }
