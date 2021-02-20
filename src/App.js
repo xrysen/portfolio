d97a6b4163eaa7ad06a87b06a754ee19bbddda41
Sean Oyler <xrysen@gmail.com>
2021-02-20 03:35:34 -0800

    Adds background and content container

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import "./App.css";
-import React, { useState } from "react";
-import Navbar from "./Components/Navbar";
-import Intro from "./Components/Intro";
-import IntroPic from "./Components/IntroPic";
-import Projects from "./Components/Projects";
-import Games from "./Components/Games";
-import Contact from "./Components/Contact";
-import { AnimateOnChange } from "react-animation";
-
-const INTRO = "INTRO";
-const PROJECTS = "PROJECTS";
-const GAMES = "GAMES";
-const CONTACT = "CONTACT";
+import React from "react";
+import me from "./assets/me.jpg";
 
 function App() {
-  const [mode, setMode] = useState(INTRO);
   return (
-    <div className="container">
-      <Navbar
-        onClick={() => setMode(PROJECTS)}
-        about={() => setMode(INTRO)}
-        intro={() => setMode(INTRO)}
-        games={() => setMode(GAMES)}
-        contact={()=> setMode(CONTACT)}
-      />
-      <AnimateOnChange
-        animationIn="fadeIn"
-        animationOut="fadeOut"
-        durationOut={200}
-      >
-        {mode === INTRO && (
-          <div className="intro-container">
-            <Intro web={() => setMode(PROJECTS)} games={()=> setMode(GAMES)} />
-            <IntroPic />
-          </div>
-        )}
-        {mode === PROJECTS && (
-          <div className="projects-container">
-            <Projects />
-          </div>
-        )}
-        {mode === GAMES && (
-          <div className="projects-container">
-            <Games />
-          </div>
-        )}
-        {mode === CONTACT && (
-          <div className = "projects-container">
-            <Contact />
-          </div>
-        )}
-      </AnimateOnChange>
+    <>
+    <div className = "top-background">
     </div>
+    <div className = "bottom-background"></div>
+    <div className = "content-container"></div>
+    </>
   );
 }
 
